ee6799ab22628eeb490a2990059649e9d7af0bba
Zidni Mubarock <zidmubarock@gmail.com>
2014-02-25 07:14:36 +0700

    write main module adn rspec init

--- active_record-when_change.gemspec
+++ active_record-when_change.gemspec
@@ -18,8 +18,9 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "activerecord", "~> 4.1.0"
+
   spec.add_development_dependency "bundler", "~> 1.5"
-  spec.add_development_dependency "railties", "~> 4.0.0"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "rr"
   spec.add_development_dependency "rake"
--- lib/active_record/when_change.rb
+++ lib/active_record/when_change.rb
@@ -2,6 +2,78 @@ require "active_record/when_change/version"
 
 module ActiveRecord
   module WhenChange
-    # Your code goes here...
+    VALID_OPTIONS = [:from, :to, :if, :unless]
+    def self.included(base)
+      base.extend ClassMethods
+      base.after_initialize do
+        @_changed_attrs = {}
+        @old_attributes = self.try(:as_json).try(:symbolize_keys).deep_dup || {}
+      end
+    end
+
+    def when_change attr, config={}, method = nil, &block
+
+      # just return if config is blank or (method or block not given)
+      return if config.blank? || (method = nil && !block_given? )
+      config.keys.each{|key| Rails.logger.debug("#{key} is not valid options key for ArctiveRecordChanged") if VALID_OPTIONS.included?(key)}
+
+      attribute_changed     = attribute_changed(attr)
+      return if attribute_changed == false
+
+      correct_form    = ( config[:from] and ( config[:from] != attribute_changed.old_value ) ? false : true )
+      correct_to      = ( config[:to] and ( config[:to] != attribute_changed.new_value ) ? false : true )
+      correct_if      = ( config[:if] and ( eval(config[:if]) != true) ? false : true )
+      correct_unless  = ( config[:unless] and (eval(config[:unless]) == true) ? false : true )
+
+      if correct_form && correct_to && correct_if && correct_unless
+        if block_given?
+          block.call(self)
+        else
+          send(method)
+        end
+      end
+
+    end
+
+    def attribute_changed(attr)
+      @_changed_attrs[attr] ||= ChangedAttributes.new(attr, @old_attributes || {}, self)
+    end
+
+    module ClassMethods
+      def when_change
+      end
+    end
+
+    class ChangedAttributes
+      attr_reader :attr, :old_attr, :current
+      def initialize attr, old_attr, current
+        @attr = attr
+        @old_attr = old_attr
+        @current = current
+      end
+
+      def inspect
+        old != value
+      end
+
+      def old
+        old_attr[attr]
+      end
+
+      def value
+        current.read_attribute attr
+      end
+
+      def old_value
+        old
+      end
+
+      def new_value
+        value
+      end
+
+    end
   end
 end
+
+ActiveRecord::Base.send(:include, ActiveRecord::WhenChange)
